ea1babba79c19a6af83bc1d5f4a1ca74a917107c
Wens10 <judebouity19@gmail.com>
2025-11-03 16:02:37 +0100

    azerty2

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <title>Wenceslas - Portfolio</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="assets/css/style.css">
+<link rel="shortcut icon" href="assets/images/alien-svgrepo-com.svg" type="image/x-icon">
 </head>
 <?php include('includes/script.php'); ?>
 <body>
